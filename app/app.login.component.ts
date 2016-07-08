08174ac15cc2717a5c3e3c8a7d6bf71a53ee3232
Ruben Laube-Pohto <h8871@student.jamk.fi>
2016-07-08 15:00:10 +0300

    Implement login
    Implemented login feature. The user is authenticated through the BaasBox REST API. Later the received session token should be saved to local storage.

--- app/app.login.component.ts
+++ app/app.login.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+
+// Add library to convert observable to promise
+import 'rxjs/add/operator/toPromise';
+
+export class LoginData {
+  username: string;
+  password: string;
+}
 
 @Component({
   selector: 'login-component',
-  // TODO: Implement proper template
-  templateUrl: 'app/templates/login.html',
+  templateUrl: 'app/templates/login.html'
 })
-export class LoginComponent { }
+export class LoginComponent {
+  data: LoginData = {
+    username: "",
+    password: ""
+  }
+
+  // TODO: Add a BaasBoxService to handle all calls to the BaasBox REST API
+
+  constructor (private http: Http, private router: Router) { }
+
+  login(username: string, password: string) {
+    console.log(username);console.log(password);
+    this.http.post('http://82.196.14.4:9000/login', {"username": username, "password": password, "appcode": '1234567890'})
+      .toPromise()
+      .then(response => {
+        // Get session token
+        let token = response.json().data['X-BB-SESSION']
+        console.log(token)
+        this.router.navigateByUrl('/status')
+      })
+      .catch(error => console.log(error))
+  }
+}
