7211c68f9c4ecc3dc2fa6c2d8bf4e567b4d07a88
laurima <H9142@student.jamk.fi>
2016-07-11 09:33:48 +0300

    Fixed buttons and flex layout

--- app/app.nav.component.ts
+++ app/app.nav.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { Router, ROUTER_DIRECTIVES } from '@angular/router';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
     selector: 'nav-component',
     templateUrl: 'app/templates/navbar.html',
     directives: [
-      ROUTER_DIRECTIVES
+      ROUTER_DIRECTIVES,
+      MD_BUTTON_DIRECTIVES
     ]
 })
 export class NavComponent {
