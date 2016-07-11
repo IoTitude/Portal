7e39848fc01c2db0295b33b4a4c5864798a4793b
laurima <H9142@student.jamk.fi>
2016-07-11 10:05:48 +0300

    Deleted legacy components

--- app/app.section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'section-component',
-    templateUrl: 'app/templates/section.html',
-})
-
-export class SectionComponent { }
